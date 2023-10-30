877c95d2bd4009baf3a01a8ca53f5d1033c15216
shruti chaube <shruti.chaube@Sportzinteractive.net>
2023-10-30 15:11:47 +0530

    First Commit

---
+++ Project 1/index.js
@@ -0,0 +1,5 @@
+// for changing single data 
+// const myjson = '{"POS":"1", "Team":"GT","P":16,"W":10,"L":4,"NR":0,"NRR":0.809,"FOR":2450,"AGAINST":2326,"PTS":20}';
+// const myObject = JSON.parse(myjson);
+// document.getElementById("json").innerHTML = myObject.Team;
+
